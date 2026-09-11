e815d2b8e5ce41cc6fbce0adfb63162ce89a11b1
Mohamed-BK1 <barkamohamed711@gmail.com>
2026-09-11 03:19:05 +0100

    Update database connection settings in login.php

--- PROJI-DARAK/login.php
+++ PROJI-DARAK/login.php
@@ -1,13 +1,19 @@
 <?php
-session_start();
+$servername = "mysql-3e6b8f9a-darak.f.aivencloud.com";
+$username   = "avnadmin";
+$password   = "AVNS_ouuoZ4E1RXO4ueR170o";
+$dbname     = "defaultdb";
+$port       = 21817;
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "darak_db";
+// إنشاء الاتصال مرة واحدة فقط
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+// التحقق من نجاح الاتصال
+if ($conn->connect_error) {
+    die("فشل الاتصال بقاعدة البيانات: " . $conn->connect_error);
+}
 
+// كود معالجة النموذج (POST) يبدأ هنا دون إعادة إنشاء $conn
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = trim($_POST['email']);
     $user_password = trim($_POST['password']);
